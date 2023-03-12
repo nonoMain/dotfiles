9f420e9a119b5e25329bd3012078ee3f5f109e4d
Noam Eliyahu <noam.e6102004@gmail.com>
2023-03-12 09:16:46 +0200

    fixed ibus

--- hyprland/.config/hypr/autostart.sh
+++ hyprland/.config/hypr/autostart.sh
@@ -3,8 +3,6 @@ sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-ibus-daemon -drx &
-/usr/lib/polkit-kde-authentication-agent-1 &
 /usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
