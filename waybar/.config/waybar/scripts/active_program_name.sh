bb0a61a9decacee1dda3d13d97495be3e4790a64
Noam Eliyahu <noam.e6102004@gmail.com>
2023-04-12 19:17:39 +0300

    renamed wezterm title name

--- waybar/.config/waybar/scripts/active_program_name.sh
+++ waybar/.config/waybar/scripts/active_program_name.sh
@@ -1,1 +1,9 @@
-hyprctl activewindow | grep -oP 'class: \K[^\s]+'
+PROG=`hyprctl activewindow | grep -oP 'class: \K[^\s]+'`
+case "$PROG" in
+	*wezterm) # wezterm has a weird name by default..
+		printf "WezTerm";
+		;;
+	*)
+		printf "$PROG";
+		;;
+esac
